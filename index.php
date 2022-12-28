a9ecefc5f5eca2473dc192296b26f1491f038310
Dianalif Aishy <112871526+dianalifaishy@users.noreply.github.com>
2022-12-28 12:42:50 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+echo "Hello World!<br>";
+$nama = "Dianalif Aishy";
+$umur = 20;
+echo "Nama saya <strong>$nama</strong>, saya berusia $umur tahun.<br>";
+$namaAdik = "Rizky";
+$umurAdik = 13;
+echo "Nama adik saya <strong>$namaAdik</strong>, adik saya berusia $umurAdik tahun.<br>";
+$selisihUmur = $umur - $umurAdik;g
+echo "Selisih usia saya dan adik saya adalah $selisihUmur tahun"
+
+?>
